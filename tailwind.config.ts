ff64861f51200685a35e623ab4d2b970de842ac2
Vladimir <volodymyr.dashkevychit@gmail.com>
2024-04-10 14:25:33 +0300

    feat: added GitHub api route

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,68 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "main-background": "var(--main-background)",
+        "secondary-background": "var(--secondary-background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        black: {
+          800: "var(--black-800)",
+          900: "var(--black-900)",
+        },
+        blue: {
+          400: "var(--blue-400)",
+          700: "var(--blue-700)",
+        },
+        green: {
+          800: "var(--green-800)",
+          300: "var(--green-300)",
+          200: "var(--green-200)",
+        },
+        gray: {
+          200: "var(--gray-200)",
+          600: "var(--gray-600)",
+          650: "var(--gray-650)",
+          700: "var(--gray-700)",
+        },
+        red: {
+          200: "var(--red-200)",
+          300: "var(--red-300)",
+          800: "var(--red-800)",
+        },
+        yellow: {
+          600: "var(--yellow-600)",
+        },
+      },
+      fontFamily: {
+        jetbrains: ["Haffer", "monospace"],
+      },
+      fontSize: {
+        "3xl": [
+          " 2rem",
+          {
+            lineHeight: "2.5rem",
+          },
+        ],
+        "4xl": [
+          "3.5rem",
+          {
+            lineHeight: "3.3rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+      },
+      boxShadow: {
+        glow: "0px 0px 8px 0px #FFFFFF14",
+      },
+      screens: {
+        "2xl": "1660px",
       },
     },
   },
   plugins: [],
 };
-export default config;
